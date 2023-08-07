b296d4b5ae89bcdfa1a282487f2196e945132160
IU7 <aolenev@bmstu.ru>
2023-08-07 21:14:55 +0300

    FIX: add gunicorn port

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def get():
     return 'hello world', 200
-
-
