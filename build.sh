276e10edea76d375c1ff6917d5c23c6b5e720efa
IU7 <aolenev@bmstu.ru>
2025-01-30 00:16:50 +0300

    FIX: remove db creation

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 source $HOME/.local/bin/env
 
-make install-uv && psql -a -d $DATABASE_URL -f database.sql
+# make install-uv && psql -a -d $DATABASE_URL -f database.sql
+make install-uv
